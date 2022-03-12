bb77a56ef2fbf87f7caa0b0561cb347ceab260b8
Nerwin Alamas <79808069+nerwinalamas@users.noreply.github.com>
2022-03-12 18:01:53 +0800

    icons

--- app/src/main/java/com/example/icctpassapp/FourthActivity.java
+++ app/src/main/java/com/example/icctpassapp/FourthActivity.java
@@ -1,15 +1,14 @@
 package com.example.icctpassapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
